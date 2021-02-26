cf62655e88e3dc34ab25e1ab0a564ebefe1c6388
jonathanleiva15 <38187976+jonathanleiva15@users.noreply.github.com>
2021-02-26 03:05:11 -0300

    ahora si

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -220,7 +220,7 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  if (numero)jonito master1
+  if (numero)jonito master1 agora sim
 
 }
 
